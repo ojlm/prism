f6176de7c9e8c81c3ad360cd407cbeb711af3c90
zhengshaodong <ngxcorpio@gmail.com>
2019-03-29 18:00:15 +0800

    A tcp echo server demo

--- src/bin/prism.rs
+++ src/bin/prism.rs
@@ -8,12 +8,38 @@ extern crate trust_dns;
 
 use std::net::SocketAddr;
 
+use tokio::io;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
 mod logger;
 
-fn main() {
+fn main() -> Result<(), Box<std::error::Error>> {
   logger::init(false, 2, prism::PRISM_NAME);
   info!("{}, {}", prism::PRISM_NAME, prism::PRISM_VERSION);
 
   let addr = "127.0.0.1:8080".to_string().parse::<SocketAddr>().unwrap();
-  info!("socks listen on: {}", addr);
+
+  let socket = TcpListener::bind(&addr)?;
+  info!("listening on: {}", addr);
+
+  let server = socket
+    .incoming()
+    .map_err(|e| error!("failed to accept socket; error = {:?}", e))
+    .for_each(move |socket| {
+      let (reader, writer) = socket.split();
+      let amount = io::copy(reader, writer);
+
+      let msg = amount.then(move |result| {
+        match result {
+          Ok((amount, _, _)) => info!("wrote {} bytes", amount),
+          Err(e) => info!("error: {}", e),
+        }
+        Ok(())
+      });
+
+      tokio::spawn(msg)
+    });
+  tokio::run(server);
+  Ok(())
 }
